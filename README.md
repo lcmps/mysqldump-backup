9db75dbc033c0f46b3451094e84b1f55449d6e6f
lcmps <lucas1498campos@gmail.com>
2022-05-09 15:27:28 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mysqldump-backup
+
+This script will backup each database on the specified MySQL server, which by default is localhost
---
+++ backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+USER="user"
+PASSWORD="password"
+HOST="localhost"
+ExcludeDatabases="Database|information_schema|performance_schema|mysql"
+databases=`mysql -u $USER -p$PASSWORD -e "SHOW DATABASES;" | tr -d "| " | egrep -v $ExcludeDatabases`
+
+for db in $databases; do
+    echo "Dumping database: $db"
+    mysqldump -h $HOST -u $USER -p$PASSWORD --databases $db > `date +%Y%m%d-%H:%m:%S`.$db.sql
+done
+
+echo "Dumping completed"
