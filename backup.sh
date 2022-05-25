5f72ecc3e255c12ae9fb2af6c0e1ff0850296249
Lucas Campos <lucas1498campos@gmail.com>
2022-05-25 19:05:18 -0300

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,11 +4,12 @@ USER="user"
 PASSWORD="password"
 HOST="localhost"
 ExcludeDatabases="Database|information_schema|performance_schema|mysql"
-databases=`mysql -u $USER -p$PASSWORD -e "SHOW DATABASES;" | tr -d "| " | egrep -v $ExcludeDatabases`
+databases=`mysql -h $HOST -u $USER -p$PASSWORD -e "SHOW DATABASES;" | tr -d "| " | egrep -v $ExcludeDatabases`
 
 for db in $databases; do
     echo "Dumping database: $db"
     mysqldump -h $HOST -u $USER -p$PASSWORD --databases $db > `date +%Y%m%d-%H:%m:%S`.$db.sql
+    sleep 5
 done
 
 echo "Dumping completed"
